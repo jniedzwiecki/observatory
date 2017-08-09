8c107b5f25980ed3d0f7410f9b79fc13183fa595
Jacek Niedzwiecki <j.niedzwiecki@oberthur.com>
2017-08-09 09:06:47 +0200

    3

--- src/test/scala/observatory/InteractionTest.scala
+++ src/test/scala/observatory/InteractionTest.scala
@@ -8,7 +8,11 @@ import scala.collection.concurrent.TrieMap
 trait InteractionTest extends FunSuite with Checkers {
 
   test("tile location") {
-    val location: Location = Interaction.tileLocation(1, 1, 1)
-    val x = 0
+    import Interaction.tileLocation
+
+    assert(Location(85.05112877980659, -180.0) == tileLocation(0, 0, 0))
+    assert(Location(85.05112877980659, -180.0) == tileLocation(1, 0, 0))
+    assert(Location(85.05112877980659, -180.0) == tileLocation(2, 0, 0))
+    assert(Location(85.05112877980659, -180.0) == tileLocation(3, 0, 0))
   }
 }
