ab1a703c3f6a0cc3c726c5f137562b523a15c603
Jacek Niedzwiecki <j.niedzwiecki@oberthur.com>
2017-08-30 00:20:43 +0200

    5. Trying to generate tiles.

--- src/main/scala/observatory/Main.scala
+++ src/main/scala/observatory/Main.scala
@@ -1,6 +1,43 @@
 package observatory
 
+import scala.collection.immutable
+
 object Main extends App {
 
+  override def main(args: Array[String]): Unit = {
+    val averageTemperatures: immutable.IndexedSeq[Iterable[(Location, Double)]] =
+      for (year <- 1975 to 1975) yield
+        Extraction.locateTemperatures(year, "/stations.csv", f"/$year.csv")
+          .map { case (date, location, temp) => (location, temp) }
 
+    val normals: (Int, Int) => Double = Manipulation.average(averageTemperatures)
+
+    val yearDeviations: Iterable[(Int, (Int, Int) => Double)] = for (year <- 1990 to 2015) yield {
+      val temperatures: Iterable[(Location, Double)] = Extraction.locateTemperatures(year, "/stations.csv", f"/$year.csv")
+        .map { case (date, location, temp) => (location, temp) }
+      (year, Manipulation.deviation(temperatures, normals))
+    }
+
+    val colors: List[(Double, Color)] = List(
+      (7, Color(0, 0, 0)),
+      (4, Color(255, 0, 0)),
+      (2, Color(255, 255, 0)),
+      (0, Color(255, 255, 255)),
+      (-2, Color(0, 255, 255)),
+      (-7, Color(0, 0, 255))
+    )
+
+    Interaction.generateTiles(
+      yearDeviations,
+      (year, zoom, x, y, data: (Int, Int) => Double) => {
+        Visualization2.visualizeGrid(
+          (i: Int, j: Int) => data(i, j) - normals(i, j),
+          colors,
+          zoom,
+          x,
+          y
+        ).output(year + ".png")
+      }
+    )
+  }
 }
